d95bca7163242bd355f914fe5da5638b04eb5c6e
Daria Borisiak <daria.borisiak@gmail.com>
2026-03-31 18:45:51 +0200

    Add swivel light control (#29)

--- unit6/ps-swivel-light-control.js
+++ unit6/ps-swivel-light-control.js
@@ -256,6 +256,8 @@ function animate() {
 function render() {
 	var delta = clock.getDelta();
 	cameraControls.update(delta);
+	light.position.x = Math.cos(effectController.angle * Math.PI / 180);
+	light.position.z = Math.sin(effectController.angle * Math.PI / 180);
 	renderer.render(scene, camera);
 }
 
